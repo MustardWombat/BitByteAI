85c71a48e229dd54c9cdaad5880ffcb11c8a3248
MustardWombat <97923953+MustardWombat@users.noreply.github.com>
2025-04-23 16:56:12 -0400

    New Shop mechanics

--- Bit/Models/PurchasedModel.swift
+++ Bit/Models/PurchasedModel.swift
@@ -11,4 +11,34 @@ struct PurchasedItem: Identifiable, Codable {
     var id: String { name }
     let name: String
     var quantity: Int
+    let type: ItemType
+    let effectValue: Double
+    var purchaseDate: Date
+    var expirationDate: Date?
+    let description: String
+    
+    var isActive: Bool {
+        if let expiration = expirationDate {
+            return Date() < expiration
+        }
+        return true // Permanent item
+    }
+    
+    var timeRemaining: String {
+        guard let expiration = expirationDate else {
+            return "Permanent"
+        }
+        
+        let timeInterval = expiration.timeIntervalSince(Date())
+        if timeInterval <= 0 {
+            return "Expired"
+        }
+        
+        let hours = Int(timeInterval / 3600)
+        if hours >= 24 {
+            let days = hours / 24
+            return "\(days)d remaining"
+        }
+        return "\(hours)h remaining"
+    }
 }
--- Bit/Models/ShopItemModel.swift
+++ Bit/Models/ShopItemModel.swift
@@ -5,10 +5,3 @@
 //  Created by James Williams on 3/24/25.
 //
 
-import Foundation
-
-struct ShopItem: Identifiable {
-    let id = UUID()
-    let name: String
-    let price: Int
-}
